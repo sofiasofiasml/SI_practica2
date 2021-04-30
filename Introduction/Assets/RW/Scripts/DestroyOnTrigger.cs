d8407fc45aef32b1f9e0c7c7e627b7632374dd81
sofia <romata1966@gmail.com>
2021-04-30 08:30:42 +0200

    juego

---
+++ Introduction/Assets/RW/Scripts/DestroyOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+    public string tagFilter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag(tagFilter)) // 2
+        {
+            Destroy(gameObject); // 3
+        }
+    }
+}
